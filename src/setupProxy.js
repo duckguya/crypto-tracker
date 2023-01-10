ccaa6ca0d31283acaf7d8aec63c74218a4a69609
duckguya <duckguya@gmail.com>
2023-01-10 15:01:33 +0900

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://api.coinpaprika.com/v1",
+      target: "https://duckguya-kr.github.io",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
