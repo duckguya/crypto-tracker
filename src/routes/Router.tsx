02fbdd2f5ba58dbae0bfd4bb7dcd366b31918db4
duckguya <duckguya@gmail.com>
2023-03-07 22:26:45 +0900

    hashrouter

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -9,7 +9,7 @@ interface IRouterProps {}
 
 function Router({}: IRouterProps) {
   return (
-    <BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/:coinId" element={<Coin />}>
@@ -18,7 +18,7 @@ function Router({}: IRouterProps) {
           <Route path="chart" element={<Chart />}></Route>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
